12c95e32a5c615c2ffe5894cda24e15b41e97a7a
Lukasz Gniecki <lukasgniecki@gmail.com>
2020-04-09 12:18:39 +0200

    Added preferred interactions. Replaced add edge on click to add edge on double click. Fixed bugs associated with user interaction with graph drawing. Vertex now spawns immediatly after the mouse is pressed, AnchorPane reacts now only to left click, not both, now its impossible to access edge mode by double clicking on empty space (Vertex appeared and after second click it entered edge mode). Edge now follows users mouse not caring if its moving freely or its dragged after second click on vertex. Fixed line thickness setting.

--- src/com/github/mtdrewski/GRAPH_moment/controller/GraphDrawer.java
+++ src/com/github/mtdrewski/GRAPH_moment/controller/GraphDrawer.java
@@ -2,6 +2,8 @@ package com.github.mtdrewski.GRAPH_moment.controller;
 
 import com.github.mtdrewski.GRAPH_moment.model.graphs.Graph;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -26,7 +28,7 @@ public class GraphDrawer {
     private Supplier<VertexCircle> vertexShapeFactory = () -> {
 
         VertexCircle vertexShape = new VertexCircle();
-        vertexShape.setStrokeDashOffset(VertexCircle.vertexStrokeDashOffset);
+        vertexShape.setStrokeWidth(VertexCircle.vertexStrokeDashOffset);
         vertexShape.setStroke(Color.BLACK);
         vertexShape.setFill(Color.WHITE); //TODO maybe add variety of colors
         vertexShape.setRadius(VertexCircle.vertexRadius);
@@ -37,16 +39,27 @@ public class GraphDrawer {
         vertexShape.setOnMouseExited(e -> {
             cursorOverVertex = false;
         });
-        vertexShape.setOnMouseClicked(e -> {
-            if (!inEdgeMode) {
-                enterEdgeMode(vertexShape);
-            }
-            else {
-                if (sourceVertex != vertexShape) {
-                    currentEdge.setEndX(vertexShape.getCenterX());
-                    currentEdge.setEndY(vertexShape.getCenterY());
-                    exitEdgeMode(true);
+        vertexShape.setOnMousePressed(e -> {
+
+            if (e.getClickCount() == 1)
+                vertexShape.clearClickCount();
+
+            vertexShape.incrementClickCount();
+
+            if (e.getButton().equals(MouseButton.PRIMARY)) {
+
+                if (!inEdgeMode && e.getClickCount() > 1 && vertexShape.getClickCount() > 1) {
+                    enterEdgeMode(vertexShape);
+                } else if (inEdgeMode) {
+                    if (sourceVertex != vertexShape) {
+                        currentEdge.setEndX(vertexShape.getCenterX());
+                        currentEdge.setEndY(vertexShape.getCenterY());
+                        exitEdgeMode(true);
+                    }
                 }
+
+                if (vertexShape.getClickCount() > 1)
+                    vertexShape.clearClickCount();
             }
         });
 
@@ -55,42 +68,48 @@ public class GraphDrawer {
 
     public void initialize() {
 
-        anchorPane.setOnMouseClicked(e -> {
+        anchorPane.setOnMousePressed(e -> {
 
-            if (!inEdgeMode) {
-                if (cursorOverVertex)
-                    return;
+            if (e.getButton().equals(MouseButton.PRIMARY)) {
+                if (!inEdgeMode) {
+                    if (cursorOverVertex)
+                        return;
 
-                double x = e.getX();
-                double y = e.getY();
-                VertexCircle vertexShape = vertexShapeFactory.get();
-                vertexShape.setCenterX(x);
-                vertexShape.setCenterY(y);
+                    double x = e.getX();
+                    double y = e.getY();
+                    VertexCircle vertexShape = vertexShapeFactory.get();
+                    vertexShape.setCenterX(x);
+                    vertexShape.setCenterY(y);
 
-                anchorPane.getChildren().add(vertexShape);
-            }
-            else {
-                if (!cursorOverVertex) {
-                    exitEdgeMode(false);
+                    anchorPane.getChildren().add(vertexShape);
+                } else {
+                    if (!cursorOverVertex) {
+                        exitEdgeMode(false);
+                    }
                 }
             }
         });
 
-        anchorPane.setOnMouseMoved(e -> {
-            if (!inEdgeMode)
-                return;
-            currentEdge.setEndX(e.getX());
-            currentEdge.setEndY(e.getY());
-        });
+        anchorPane.setOnMouseMoved(this::edgeFollowCursor);
+        anchorPane.setOnMouseDragged(this::edgeFollowCursor);
+    }
+
+    private void edgeFollowCursor(MouseEvent e) {
+        if (!inEdgeMode)
+            return;
+        currentEdge.setEndX(e.getX());
+        currentEdge.setEndY(e.getY());
     }
 
     private void enterEdgeMode(VertexCircle vertexShape) {
 
         EdgeLine edgeShape = new EdgeLine();
+        edgeShape.setStrokeWidth(EdgeLine.thickness);
         edgeShape.setStartX(vertexShape.getCenterX());
         edgeShape.setStartY(vertexShape.getCenterY());
         edgeShape.setEndX(edgeShape.getStartX());
         edgeShape.setEndY(edgeShape.getStartY());
+
         currentEdge = edgeShape;
         anchorPane.getChildren().add(0, edgeShape);
         inEdgeMode = true;
@@ -106,13 +125,21 @@ public class GraphDrawer {
 
     private static class VertexCircle extends Circle {
 
-        private static double vertexRadius = 20;
-        private static double vertexStrokeDashOffset = 5;
+        private static double vertexRadius = 20.0;
+        private static double vertexStrokeDashOffset = 4.0;
+
+        private int clickCount = 0;
+
+        public int getClickCount() { return clickCount; }
+        public void incrementClickCount() { clickCount++; }
+        public void clearClickCount() { clickCount = 0; }
 
         private HashSet<EdgeLine> outcomingEdges;
     }
 
     private static class EdgeLine extends Line {
+
+        private static double thickness = 4.0;
 
         private VertexCircle startVertex;
         private VertexCircle endVertex;
