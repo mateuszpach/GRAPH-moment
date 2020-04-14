b90e4ab039ef1415273ae62fe2055e038121c618
Mateusz Pach <mateusz.pach.05@gmail.com>
2020-04-14 12:54:51 +0200

    Icon added.

--- src/com/github/mtdrewski/GRAPH_moment/Main.java
+++ src/com/github/mtdrewski/GRAPH_moment/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,6 +17,7 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.setMinWidth(800);
         primaryStage.setMinHeight(500);
+        primaryStage.getIcons().add(new Image("icon.png"));
         primaryStage.show();
 
     }
