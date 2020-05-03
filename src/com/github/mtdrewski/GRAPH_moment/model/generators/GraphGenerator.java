f077465edb2b5d794f46c9502e94eb5a4a652f52
Lukasz Gniecki <lukasgniecki@gmail.com>
2020-05-03 20:57:43 +0200

    Added random graph generator interface, added abstract class for an interval constrained graph and added class for standard graph random generation.
    Passed unit test (created by me)

---
+++ src/com/github/mtdrewski/GRAPH_moment/model/generators/GraphGenerator.java
@@ -0,0 +1,7 @@
+package com.github.mtdrewski.GRAPH_moment.model.generators;
+
+import com.github.mtdrewski.GRAPH_moment.model.graphs.Graph;
+
+public interface GraphGenerator {
+    Graph generate();
+}
---
+++ src/com/github/mtdrewski/GRAPH_moment/model/generators/IntervalConstrainedGenerator.java
@@ -0,0 +1,19 @@
+package com.github.mtdrewski.GRAPH_moment.model.generators;
+
+public abstract class IntervalConstrainedGenerator implements GraphGenerator {
+
+    protected int minNumOfVertices;
+    protected int minNumOfEdges;
+
+    protected int maxNumOfVertices;
+    protected int maxNumOfEdges;
+
+    public IntervalConstrainedGenerator(int minV, int maxV, int minE, int maxE) throws IllegalArgumentException {
+        if (minV < 1 || minE < 0 || maxV < minV || maxE < minE || minE > minV * (minV - 1) / 2)
+            throw new IllegalArgumentException();
+        minNumOfVertices = minV;
+        minNumOfEdges = minE;
+        maxNumOfVertices = maxV;
+        maxNumOfEdges = maxE;
+    }
+}
