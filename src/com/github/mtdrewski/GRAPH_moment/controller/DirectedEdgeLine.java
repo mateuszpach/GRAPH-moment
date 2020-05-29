d94d75695cdb9d082a923d36bad44293746ce02b
mtdrewski <mateusz.olszewski5040@gmail.com>
2020-05-29 11:31:57 +0200

    First draft of semi working directed edges implementations

---
+++ src/com/github/mtdrewski/GRAPH_moment/controller/DirectedEdgeLine.java
@@ -0,0 +1,39 @@
+package com.github.mtdrewski.GRAPH_moment.controller;
+
+import javafx.beans.binding.DoubleBinding;
+import javafx.scene.shape.Polygon;
+import javafx.scene.transform.Rotate;
+
+
+public class DirectedEdgeLine extends EdgeLine{
+
+    private Polygon triangle;
+    private DoubleBinding dx;
+    private DoubleBinding dy;
+
+    public DirectedEdgeLine(GraphDrawerController drawer) {
+        super(drawer);
+        dx = endXProperty().add(startXProperty().negate());
+        dy = endYProperty().add(startYProperty().negate());
+        triangle = new Polygon(getEndX(), getEndY(), getEndX() - 16, getEndY() + 8, getEndX() - 16, getEndY() - 8);
+        var rotate = new Rotate(0,0,0,1,Rotate.Z_AXIS);
+
+        triangle.getTransforms().add(rotate);
+        dx.addListener((observable, oldValue, newValue) -> {
+            rotate.setAngle(getAngle(dy.doubleValue(), newValue.doubleValue()));
+        });
+        dy.addListener((observable, oldValue, newValue) -> {
+            rotate.setAngle(getAngle(newValue.doubleValue(), dx.doubleValue()));
+        });
+
+        triangle.layoutXProperty().bind(endXProperty());
+        triangle.layoutYProperty().bind(endYProperty());
+        graphDrawerController.root.getChildren().add(triangle);
+    }
+
+
+    private double getAngle(double dy ,double dx){
+        return Math.toDegrees(Math.atan2(dy, dx));
+    }
+
+}
