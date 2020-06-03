e363a427184f172bb568f5666017c57e8b9aa40a
Lukasz Gniecki <lukasgniecki@gmail.com>
2020-06-03 05:32:10 +0200

    Changed implementation of graphs to fit better further changes for making avaliable selecting edges and later labels on edges

---
+++ src/com/github/mtdrewski/GRAPH_moment/model/graphs/DirectedEdge.java
@@ -0,0 +1,26 @@
+package com.github.mtdrewski.GRAPH_moment.model.graphs;
+
+import java.util.Objects;
+
+public class DirectedEdge extends Edge {
+
+    protected DirectedEdge(Vertex u, Vertex v) {
+        super(u, v);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Edge edge = (Edge) o;
+        return Objects.equals(vertex1, edge.vertex1) &&
+                Objects.equals(vertex2, edge.vertex2);
+    }
+
+    @Override
+    public void swap() { // useful for directed edge randomization
+        Vertex temp = vertex1;
+        vertex1 = vertex2;
+        vertex2 = temp;
+    }
+}
