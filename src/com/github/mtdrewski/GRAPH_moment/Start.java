016eda2519c88d693d95f0aae1dc925adf2c70fc
mateuszpach <mateusz.pach.05@gmail.com>
2020-07-22 18:25:11 +0200

    Rebuilt with gradle. Changed an icons library to fontawesomefx. It is possible to create a shadowJar now.

--- src/com/github/mtdrewski/GRAPH_moment/Start.java
+++
@@ -1,7 +0,0 @@
-package com.github.mtdrewski.GRAPH_moment;
-
-public class Start {
-    public static void main(String[] args){
-        Main.main(args);
-    }
-}
--- src/com/github/mtdrewski/GRAPH_moment/model/generators/GraphGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.github.mtdrewski.GRAPH_moment.model.generators;
-
-import com.github.mtdrewski.GRAPH_moment.model.graphs.Graph;
-
-public interface GraphGenerator {
-    Graph generate();
-}
